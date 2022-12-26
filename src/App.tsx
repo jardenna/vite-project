8c39e9f5b2358fb730359ac3b4016476160e15f1
jardenna <hbjornum@gmail.com>
2022-12-26 10:37:27 +0100

    fixed eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 function App() {
-  return <h1>hello</h1>;
+  const onClick = () => {
+    console.log(145);
+  };
+  return (
+    <section>
+      <h1>hello</h1>
+      <img src="" alt="img" />
+      <button type="button" onClick={onClick}>
+        <div onClick={onClick} role="button" tabIndex={0}>
+          klik
+        </div>
+        Klik
+      </button>
+    </section>
+  );
 }
 
 export default App;
