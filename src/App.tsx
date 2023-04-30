2885054f22636f70a23e38826c9c585b98f4af59
jardenna <hbjornum@gmail.com>
2023-04-30 12:45:39 +0200

    design

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
-import Images from './components/Images';
 import Header from './layout/Header';
 
 function App() {
   return (
     <article className="main-container">
       <Header />
-      <main className="main-content">hello</main>
+
+      <div className="container">
+        <main className="main-content">
+          <aside className="aside">
+            <span className="vertical-line-container">
+              <span className="vertical-line" />
+              <span className="vertical-line" />
+            </span>
+            <div>
+              <h1>Images</h1>
+              <div>
+                Seek out exclusions, and use them as opportunities to create new
+                and better experiences.
+                <p>
+                  <a href="https://www.microsoft.com/design">Microsoft</a>
+                </p>
+              </div>
+            </div>
+          </aside>
+
+          <section>
+            Bear in mind that a lot of accessibility best practices benefit
+            everyone, not just users with disabilities. For example, lean
+            semantic markup is not only good for screen readers, but it is also
+            fast to load and performant. This benefits everyone, especially
+            those on mobile devices and/or slow connections.
+          </section>
+        </main>
+      </div>
 
       <footer className="main-footer">
-        <div className="container">Footer</div>
+        <div className="container">
+          <nav className="footer-nav">Contact Great links Thanks</nav>
+          <span className="copyright">Copyright 2023</span>
+        </div>
       </footer>
     </article>
   );
