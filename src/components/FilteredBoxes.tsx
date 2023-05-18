fac10d149be6491a50329300480fc0da35e5d11f
jardenna <hbjornum@gmail.com>
2023-05-18 13:37:56 +0200

    added delete

--- src/components/FilteredBoxes.tsx
+++ src/components/FilteredBoxes.tsx
@@ -59,104 +59,93 @@ const FilteredSelectBoxes = () => {
       id: 1,
       title: 'Leg 1',
       component: (
-        <select value={selectedOption1} onChange={handleSelectOption1}>
-          {options.map((option: Option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
+        <>
+          <select value={selectedOption1} onChange={handleSelectOption1}>
+            {options.map((option: Option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+          <section>Section 1</section>
+          <section>Section 2</section>
+          <section>Section 3</section>
+        </>
       ),
     },
     {
       id: 2,
       title: 'Leg 2',
       component: (
-        <select value={selectedOption2} onChange={handleSelectOption2}>
-          {options1.map((option: Option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
-      ),
-    },
-    // {
-    //   id: 3,
-    //   title: 'Leg 3',
-    //   component: (
-    //     <select value={selectedOption3} onChange={handleSelectOption3}>
-    //       {remainingOptions2.map((option: Option) => (
-    //         <option key={option.value} value={option.value}>
-    //           {option.label}
-    //         </option>
-    //       ))}
-    //     </select>
-    //   ),
-    // },
-    // {
-    //   id: 4,
-    //   title: 'Leg 4',
-    //   component: (
-    //     <select>
-    //       {remainingOptions3.map((option: Option) => (
-    //         <option key={option.value} value={option.value}>
-    //           {option.label}
-    //         </option>
-    //       ))}
-    //     </select>
-    //   ),
-    // },
-  ];
-  return (
-    <div>
-      <div className="grid test grid-header">
-        <div>Departure</div>
-        <div>Arrival</div>
-      </div>
-      {legs.map((leg) => (
-        <fieldset key={leg.id} className="grid test">
-          <legend> {leg.title}</legend>
-
-          <section className="pr-40"> {leg.component}</section>
+        <>
+          <select value={selectedOption2} onChange={handleSelectOption2}>
+            {options1.map((option: Option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
           <section>Section 1</section>
           <section>Section 2</section>
           <section>Section 3</section>
+        </>
+      ),
+    },
+    {
+      id: 3,
+      title: 'Leg 3',
+      component: (
+        <>
+          <select value={selectedOption3} onChange={handleSelectOption3}>
+            {remainingOptions2.map((option: Option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+          <section>Section 1</section>
+          <section>Section 2</section>
+          <section>Section 3</section>
+        </>
+      ),
+    },
+    {
+      id: 4,
+      title: 'Leg 4',
+      component: (
+        <>
+          <select>
+            {remainingOptions3.map((option: Option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+          <section>Section 1</section>
+          <section>Section 2</section>
+          <section>Section 3</section>
+        </>
+      ),
+    },
+  ];
+  return (
+    <div>
+      <div className="grid test1 grid-header">
+        <div className="grid test col-start-2">
+          <div>Departure</div>
+          <div className="col-start-3">Arrival</div>
+        </div>
+      </div>
+      {legs.map((leg, index: number) => (
+        <fieldset key={leg.id} className="grid test1">
+          <legend> {leg.title}</legend>
+          <div className="grid test">{leg.component}</div>
+          {legs.length - 1 === index && index !== 0 && (
+            <button type="button">Delete</button>
+          )}
         </fieldset>
       ))}
     </div>
-    // <div>
-    //   <select value={selectedOption1} onChange={handleSelectOption1}>
-    //     {options.map((option: Option) => (
-    //       <option key={option.value} value={option.value}>
-    //         {option.label}
-    //       </option>
-    //     ))}
-    //   </select>
-
-    //   <select value={selectedOption2} onChange={handleSelectOption2}>
-    //     {options1.map((option: Option) => (
-    //       <option key={option.value} value={option.value}>
-    //         {option.label}
-    //       </option>
-    //     ))}
-    //   </select>
-    //   <select value={selectedOption3} onChange={handleSelectOption3}>
-    //     {remainingOptions2.map((option: Option) => (
-    //       <option key={option.value} value={option.value}>
-    //         {option.label}
-    //       </option>
-    //     ))}
-    //   </select>
-
-    //   <select>
-    //     {remainingOptions3.map((option: Option) => (
-    //       <option key={option.value} value={option.value}>
-    //         {option.label}
-    //       </option>
-    //     ))}
-    //   </select>
-    // </div>
   );
 };
 
